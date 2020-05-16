52c9786684b9ab4e8613b5befac1d7977a775829
daerd <daniel.herrero.101@gmail.com>
2020-05-16 17:16:03 +0200

    NodeModules volume added in Gatsby config.

--- NodeJS & Gatsby/docker/entrypoint.sh
+++ NodeJS & Gatsby/docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/ash
 
+# We keep the Gatsby modules installed in the build phase and symlink the new ones in every run via entrypoint.
 ln -s /save/node_modules/* ./node_modules/.
 
 gatsby develop -H 0.0.0.0
